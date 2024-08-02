0a66dc28c9a6e490ee800fbf44754256c058f0b7
Delphinoid <penganddolph@gmail.com>
2024-08-02 17:32:19 +1000

    Added blog post about arpeggios.

--- blog/posts.js
+++ blog/posts.js
@@ -3,6 +3,7 @@ const POST_URL_PREFIX = "./blog/";
 
 // Array of tags for each post, stored from newest to oldest. The last tag is the post ID.
 const POSTS = [
+	["2024/08/02", "life", "music", "theory", "3"],
 	["2024/06/05", "life", "mathematics", "phd", "2"],
 	["2024/03/19", "life", "mathematics", "phd", "1"],
 	["2024/03/16", "hello", "life", "miscellaneous", "programming", "website", "world", "0"]
