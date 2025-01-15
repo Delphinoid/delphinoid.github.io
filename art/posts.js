4dc6972b6e353b94d10eb4a0d806ea1d7892f3f5
Delphinoid <penganddolph@gmail.com>
2025-01-15 21:12:18 +1100

    Added temporary file for sharing something.

--- art/posts.js
+++ art/posts.js
@@ -3,6 +3,7 @@ const POST_URL_PREFIX = "./art/";
 
 // Array of tags for each post, stored from newest to oldest. The last tag is the post ID.
 const POSTS = [
+	["2025/15/01", "3d", "character", "desktop", "mate", "luka", "megurine", "vrm", "vroid", "4"],
 	["2023/11/04", "2d", "character", "oc", "weird", "3"],
 	["2021/08/17", "2d", "character", "oc", "rigged", "modelling", "vtuber", "2"],
 	["2021/08/17", "3d", "character", "oc", "rigged", "modelling", "1"],
