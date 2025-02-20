72160ce8a848a63c439c72084dfc14abccd7820a
Delphinoid <penganddolph@gmail.com>
2025-02-20 12:59:50 +1100

    Added random sketches. Probably temporary.

--- art/posts.js
+++ art/posts.js
@@ -3,8 +3,9 @@ const POST_URL_PREFIX = "./art/";
 
 // Array of tags for each post, stored from newest to oldest. The last tag is the post ID.
 const POSTS = [
+	["2025/20/02", "2d", "character", "oc", "luka", "megurine", "5"],
 	["2025/15/01", "3d", "character", "desktop", "mate", "luka", "megurine", "vrm", "vroid", "4"],
-	["2023/11/04", "2d", "character", "oc", "weird", "3"],
+	["2024/11/04", "2d", "character", "oc", "weird", "3"],
 	["2021/08/17", "2d", "character", "oc", "rigged", "modelling", "vtuber", "2"],
 	["2021/08/17", "3d", "character", "oc", "rigged", "modelling", "1"],
 	["2018/05/23", "3d", "cappingtv", "cptv", "downunder", "lan", "medals", "modelling", "tf2", "0"]
