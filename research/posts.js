41eee53301fa7b8e97b45eb9310fe3d7fae6eb3e
Delphinoid <penganddolph@gmail.com>
2025-05-09 21:58:20 +1000

    Added some notes from an informal talk I gave.

--- research/posts.js
+++ research/posts.js
@@ -3,10 +3,11 @@ const POST_URL_PREFIX = "./research/";
 
 // Array of tags for each post, stored from newest to oldest. The last tag is the post ID.
 const POSTS = [
+	["2025/05/08", "a_1", "a_2", "a1", "a2", "bimodules", "indecomposable", "soergel", "type", "11"],
 	["2025/04/07", "algebras", "amenable", "amenability", "analysis", "asymptotic", "automata", "banach", "cellular", "decomposition", "ergodic", "group", "hausdorff", "homological", "neumann", "operator", "paradoxical", "slides", "talks", "tarski", "theory", "unsw", "von", "10"],
 	["2025/02/28", "admissible", "algebraic", "algebras", "birepresentations", "categorical", "categories", "category", "categorification", "groups", "kazhdan", "lusztig", "modules", "polynomial", "real", "reductive", "representations", "slides", "talks", "vogan", "unsw", "9"],
 	["2024/12/05", "algebras", "birepresentations", "categorical", "categories", "category", "categorification", "cohomology", "homology", "jones", "khovanov", "modules", "monoidal", "polynomial", "representations", "slides", "talks", "wsu", "8"],
-	["2024/06/18", "algebras", "birepresentations", "categorical", "categories", "category", "holder", "hölder", "jordan", "modules", "monoidal", "representations", "talks", "7"],
+	["2024/06/18", "algebras", "bimodules", "birepresentations", "categorical", "categories", "category", "cell", "holder", "hölder", "jordan", "lusztig", "modules", "monoidal", "representations", "soergel", "talks", "theory", "vogan", "7"],
 	["2024/05/01", "algebras", "birepresentations", "categories", "category", "modules", "monoidal", "representations", "subfactors", "tensor", "6"],
 	["2024/05/01", "algebras", "birepresentations", "categories", "category", "modules", "monoidal", "representations", "slides", "subfactors", "talks", "tensor", "5"],
 	["2023/11/16", "algebras", "arnaud", "brothier", "category", "conformal", "groups", "honours", "jones", "monoidal", "operad", "operator", "planar", "quantum", "richard", "subfactors", "thompson", "unsw", "vaughan", "4"],
