6d208fe6f3031430a3e54fa426c5a750ff8dce75
Delphinoid <penganddolph@gmail.com>
2025-09-25 13:22:54 +1000

    Added some random drawings and stuff.

--- art/posts.js
+++ art/posts.js
@@ -3,9 +3,10 @@ const POST_URL_PREFIX = "./art/";
 
 // Array of tags for each post, stored from newest to oldest. The last tag is the post ID.
 const POSTS = [
+	["2025/09/25", "website", "9"],
 	["2025/09/25", "2d", "character", "oc", "old", "8"],
 	["2025/09/25", "2d", "character", "oc", "7"],
-	["2025/09/25", "website", "6"],
+	["2025/03/23", "2d", "character", "oc", "6"],
 	["2025/02/20", "2d", "character", "oc", "luka", "megurine", "5"],
 	["2025/01/15", "3d", "character", "desktop", "mate", "luka", "megurine", "vrm", "vroid", "4"],
 	["2024/11/04", "2d", "character", "oc", "weird", "3"],
